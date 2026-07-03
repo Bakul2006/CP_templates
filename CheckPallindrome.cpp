f76dcb5105f379674b83a89169fd5aefcbfbd3c9
Bakul Bansal <24uec277@lnmiit.ac.in>
2026-07-03 22:39:19 +0530

    Update CheckPallindrome.cpp

--- CheckPallindrome.cpp
+++ CheckPallindrome.cpp
@@ -7,3 +7,30 @@ bool isPalindrome(vector<int>& a, int l, int r) {
     }
     return true;
 }
+
+
+//checking with expanding through center
+string longestPalindrome(string s) {
+    int n = s.size();
+
+    int start = 0;
+    int len = 1;
+
+    auto expand = [&](int l, int r) {
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            if (r - l + 1 > len) {
+                len = r - l + 1;
+                start = l;
+            }
+            l--;
+            r++;
+        }
+    };
+
+    for (int i = 0; i < n; i++) {
+        expand(i, i);       // odd
+        expand(i, i + 1);   // even
+    }
+
+    return s.substr(start, len);
+}
